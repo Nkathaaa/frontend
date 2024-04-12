3e1f43cf815f8bb75708f78a085cf8f27d8cedd5
Nkathaaa <catherinesheilankatha@gmail.com>
2024-04-12 13:04:16 +0300

    sorted eb 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        please dont do that <code>src/App.js</code> and save to reload.This is valid
+        please dont do that <code>src/App.js</code> and save to reload.This is validddd
         </p>
         <a
           className="App-link"
