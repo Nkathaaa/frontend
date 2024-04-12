41547e75dc2390da018a6f7f92275ed0c1fcd40c
Nkathaaa <catherinesheilankatha@gmail.com>
2024-04-12 15:43:51 +0300

    deployed well

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        please dont do that <code>src/App.js</code> and save to reload.This is validddd
+        please dont do that <code>src/App.js</code> and save to reload.This is vxcalidddd
         </p>
         <a
           className="App-link"
