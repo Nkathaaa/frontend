79bf67149a3905bee197ca4c8b67dd0b13c2d7ba
Nkathaaa <catherinesheilankatha@gmail.com>
2024-05-02 10:14:39 +0300

    sorted eb 12

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        please dont do that <code>src/App.js</code> and save to reload.This is valid to an extent much
+        please dont do that <code>src/App.js</code> and save to reload.This is you
         <a
           className="App-link"
           href="https://reactjs.org"
