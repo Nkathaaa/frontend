dc8646ae9daed4f7f3f130e2be0d009e5586dd8f
Nkathaaa <catherinesheilankatha@gmail.com>
2024-05-08 13:22:49 +0300

    sorted eb 28

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        please dont do that <code>src/App.js</code> and save to   Sheila Nkatha
+        please dont do that <code>src/App.js</code> and save to   Sheila Nkatha She
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
